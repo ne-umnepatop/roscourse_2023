7b8385812318d3fea4c02c471b3dc07884e15895
ne-umnepatop <andrei.zelepugin@yandex.ru>
2024-01-20 23:22:41 +0300

    best speed constants

--- scripts/lidar_processing.py
+++ scripts/lidar_processing.py
@@ -3,10 +3,12 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
 
-
+BASIC_SPEED = -21
 ACCELERATION = -5
 K = 300
-BOOST = -30
+BOOST = -35
+THRESHOLD_DISTANCE = 0.72
+
 
 
 class ObstacleAvoidance:
@@ -18,34 +20,33 @@ class ObstacleAvoidance:
         self.right_wheel_controller = rospy.Publisher('/project_robot/right_wheel_controller/command', Float64, queue_size=1)
         self.left_wheel_controller = rospy.Publisher('/project_robot/left_wheel_controller/command', Float64, queue_size=1)
 
-        self.joint_speed = -20.0 
-        self.obstacle_distance_threshold = 0.7
+        BASIC_SPEED = -20.0 
 
     def move_forward(self):
-        self.right_wheel_controller.publish(self.joint_speed)
-        self.left_wheel_controller.publish(self.joint_speed)
+        self.right_wheel_controller.publish(BASIC_SPEED)
+        self.left_wheel_controller.publish(BASIC_SPEED)
 
     def move_back(self):
-        joint_speed = 4.0  
-        self.right_wheel_controller.publish(joint_speed)
-        self.left_wheel_controller.publish(joint_speed)
+        backward_joint_speed = 4.0
+        self.right_wheel_controller.publish(backward_joint_speed)
+        self.left_wheel_controller.publish(backward_joint_speed)
 
     def turn_left(self,a): 
         rospy.logwarn(f'turning left, {K*a}')
-        self.right_wheel_controller.publish(self.joint_speed+ACCELERATION)
-        self.left_wheel_controller.publish(-self.joint_speed)
+        self.right_wheel_controller.publish(BASIC_SPEED+ACCELERATION)
+        self.left_wheel_controller.publish(-BASIC_SPEED)
 
     def turn_right(self,a):
         rospy.logwarn(f'turning right, {K*a}')
-        self.right_wheel_controller.publish(-self.joint_speed)
-        self.left_wheel_controller.publish(self.joint_speed+a*K*ACCELERATION)
+        self.right_wheel_controller.publish(-BASIC_SPEED)
+        self.left_wheel_controller.publish(BASIC_SPEED+a*K*ACCELERATION)
 
     def lidar_callback(self, data):
         # distance_threshold = 2.0  
-        if data.ranges[0] < self.obstacle_distance_threshold:
-            self.turn_right(self.obstacle_distance_threshold-data.ranges[0])      
-        elif data.ranges[-1] < self.obstacle_distance_threshold:
-            self.turn_left(self.obstacle_distance_threshold-data.ranges[-1])
+        if data.ranges[0] < THRESHOLD_DISTANCE:
+            self.turn_right(THRESHOLD_DISTANCE-data.ranges[0])      
+        elif data.ranges[-1] < THRESHOLD_DISTANCE:
+            self.turn_left(THRESHOLD_DISTANCE-data.ranges[-1])
         else:
             if data.ranges[5] > 3:
                 self.right_wheel_controller.publish(BOOST)
