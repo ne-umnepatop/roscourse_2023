234ba0ac52f41df00296bb5d6adfba0be36d7576
ne-umnepatop <andrei.zelepugin@yandex.ru>
2024-01-23 14:16:20 +0300

    Exta code deleted

--- scripts/lidar_processing.py
+++ scripts/lidar_processing.py
@@ -3,7 +3,10 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
 
-add_vel = 6 # no more daddy please
+ACCELERATION = 6 # no more daddy please
+BASIC_SPEED = 25.0 
+THRESHOLD_DISTANCE  = 0.8
+BOOST = 35
 
 ns = 'project_robot_368201'
 
@@ -24,8 +27,7 @@ class ObstacleAvoidance:
         self.left_wheel_controller = rospy.Publisher(
             f'/{ns}/left_wheel_controller/command', Float64, queue_size=1)
 
-        self.joint_speed = 25.0 
-        self.obstacle_distance_threshold = 0.8
+
 
     def move_forward(self, speed):
         self.right_wheel_controller.publish(-speed)
@@ -37,27 +39,27 @@ class ObstacleAvoidance:
         self.left_wheel_controller.publish(joint_speed)
 
     def turn_left(self, dist):
-        self.right_wheel_controller.publish(-self.joint_speed-add_vel)
-        self.left_wheel_controller.publish(self.joint_speed)
+        self.right_wheel_controller.publish(-BASIC_SPEED-ACCELERATION)
+        self.left_wheel_controller.publish(BASIC_SPEED)
 
     def turn_right(self, dist):
-        self.right_wheel_controller.publish(self.joint_speed)
-        self.left_wheel_controller.publish(-self.joint_speed-add_vel)
+        self.right_wheel_controller.publish(BASIC_SPEED)
+        self.left_wheel_controller.publish(-BASIC_SPEED-ACCELERATION)
 
     def lidar_callback(self, data):
         rospy.logwarn(
             f'dist: left:{data.ranges[-1]} mid:{data.ranges[4]} right:{data.ranges[0]}')
 
         if abs(data.ranges[4]) > 3 and round_int(data.ranges[0]) >= 1 and round_int(data.ranges[-1]) >= 1:
-            self.move_forward(35)
-        elif data.ranges[0] < self.obstacle_distance_threshold:
+            self.move_forward(BOOST)
+        elif data.ranges[0] < THRESHOLD_DISTANCE :
             self.turn_right(data.ranges[0])
-        elif data.ranges[-1] < self.obstacle_distance_threshold:
+        elif data.ranges[-1] < THRESHOLD_DISTANCE :
             self.turn_left(data.ranges[-1])
-        elif sum(data.ranges) < self.obstacle_distance_threshold*9:
+        elif sum(data.ranges) < THRESHOLD_DISTANCE *9:
             self.move_back()
         else:
-            self.move_forward(self.joint_speed)
+            self.move_forward(BASIC_SPEED)
 
 
 def main():
